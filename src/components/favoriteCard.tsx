c50b1ad7505c3a2b8113a3a194fc78a5bcdf8f1f
Manuel Alabor <msites@msites.net>
2015-10-26 22:49:48 +0100

    tryout with favorite based progress indicator

--- src/components/favoriteCard.tsx
+++ src/components/favoriteCard.tsx
@@ -2,7 +2,7 @@
 /// <reference path="../../typings/material-ui/material-ui.d.ts" />
 
 import * as React from 'react';
-import { ListItem, MenuItem, IconMenu, IconButton, Avatar, FontIcon } from 'material-ui';
+import { CircularProgress, ListItem, MenuItem, IconMenu, IconButton, Avatar, FontIcon } from 'material-ui';
 import { Favorite } from '../reducers/favorites';
 import * as Store from '../store/index';
 
@@ -15,12 +15,21 @@ const CARD_STYLE = { 'margin': '16px' };
 
 export default class FavoriteCard extends React.Component<FavoriteCardProps, any> {
     render() {
-        let title = this.props.favorite.from.name + ' -> ' + this.props.favorite.to.name + this.props.favorite.isFetching;
+        let title = this.props.favorite.from.name + ' -> ' + this.props.favorite.to.name;
         let nextDeparture = this.props.favorite.nextConnections[0].from.departure.toString();
         let icon = <FontIcon className="material-icons">{ this.props.favorite.icon }</FontIcon>;
         let avatar = <Avatar icon={ icon } />;
 
-        let iconButton = <IconButton onClick={ this.props.onClick }>{ icon }</IconButton>;
+        let iconButton;
+        if(this.props.favorite.isFetching) {
+            iconButton = <CircularProgress size={0.5} mode="indeterminate" />
+        } else {
+            iconButton = (
+                <IconButton onClick={ this.props.onClick }>
+                    <FontIcon className="material-icons">swap_horiz</FontIcon>
+                </IconButton>
+            );
+        }
 
         return (
             <ListItem
